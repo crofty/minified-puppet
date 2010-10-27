7b9aa5e0dc0aaaf1fb966c27a858f0330e80eae1
James Croft <James.R.Croft@googlemail.com>
2010-10-27 09:15:45 +0700

    added munin plugins for rails request log

--- puppet/classes/munin.pp
+++ puppet/classes/munin.pp
@@ -20,4 +20,52 @@ class munin {
     }
   }
 
+  define plugin($config, $content) {
+    include munin::client
+
+    file {"/etc/munin/plugins/$name":
+      content => $content,
+      mode => 777,
+      require => Package["munin-node"],
+      notify => Service["munin-node"]
+    }
+
+    file {"/etc/munin/plugin-conf.d/$name":
+      content => "[$name]\n$config",
+      require => Package["munin-node"],
+      notify => Service["munin-node"]
+    }
+  }
+
+
+  define rails($log) {
+
+    package {"request-log-analyzer":
+      ensure => present,
+      provider => gem
+    }
+
+    munin::plugin {"$name-rails-requests":
+      config => template("munin/rails-plugin-config"),
+      content => template("munin/plugins/rails_requests")
+    }
+
+    munin::plugin {"$name-rails-request-duration":
+      config => template("munin/rails-plugin-config"),
+      content => template("munin/plugins/rails_request_duration")
+    }
+
+    munin::plugin {"$name-rails-request-error":
+      config => template("munin/rails-plugin-config"),
+      content => template("munin/plugins/rails_request_error")
+    }
+
+    munin::plugin {"$name-rails-view-render-time":
+      config => template("munin/rails-plugin-config"),
+      content => template("munin/plugins/rails_view_render_time")
+    }
+  }
+
+
+
 }
