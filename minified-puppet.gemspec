e326329087e21a41e92be9ff27483a1bf53aa0f3
James Croft <James.R.Croft@googlemail.com>
2010-12-17 11:47:11 +0800

    added nodejs

--- minified-puppet.gemspec
+++ minified-puppet.gemspec
@@ -2,11 +2,11 @@
 
 Gem::Specification.new do |s|
   s.name = %q{minified-puppet}
-  s.version = "0.0.16"
+  s.version = "0.0.17"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["James Croft"]
-  s.date = %q{2010-11-17}
+  s.date = %q{2010-12-17}
   s.email = %q{james.r.croft@gmail.com}
   s.files = [
     ".gitignore",
---
+++ puppet/classes/nodejs.pp
@@ -0,0 +1,43 @@
+class nodejs {
+
+  define nodejs_source(
+    $version = 'v0.2.5',
+    $path = '/usr/local/src',
+    $bin = '/usr/local/bin'
+  ) {
+
+    file { nodejs_folder:
+      path => "${path}/nodejs_${version}",
+      ensure => "directory",
+      owner => root,
+      group => root
+    }
+
+    exec { nodejs_code: 
+      command =>"wget http://nodejs.org/dist/node-${version}.tar.gz -O nodejs_${version}.tar.gz && tar --strip-components 1 -xzvf nodejs_${version}.tar.gz",
+      cwd => "${path}/nodejs_${version}",
+      creates => "${path}/nodejs_${version}/Makefile",
+      require => File["nodejs_folder"],
+      before => Exec["make ${version}"]
+    }
+
+    file { "${path}/nodejs_${version}/nodejs_${version}.tar.gz":
+      ensure => absent,
+      require => Exec["nodejs_code"]
+    }
+    exec { "make ${version}":
+      command => "cd ${path}/nodejs_${version} && ./configure && make && make install",
+      creates => "${bin}/node",
+    }
+  }
+  
+  user { "nodejs":
+    ensure => present
+  }
+
+  nodejs_source {"nodejs_source":
+    require => User["nodejs"]
+  }
+}
+
+
