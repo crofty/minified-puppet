93ffaaca7fb324ae914147cfafb0e781cfe5aebb
James Croft <james.r.croft@gmail.com>
2011-03-31 19:23:48 +0100

    added postfix back into the base setup

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -1,5 +1,6 @@
 # NOTE: before running this, be sure to add the following line to /etc/apt/sources.list
 # deb http://backports.debian.org/debian-backports lenny-backports main contrib non-free
+# You might want to remove this after puppet has been installed
 sudo apt-get remove ruby
 sudo aptitude update
 sudo apt-get -y update
